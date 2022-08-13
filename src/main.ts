4a2dfa20c6dcd95f3e61fcad159dfe0a91bda649
shahan953 <misbauddin1994@gmail.com>
2022-08-13 12:45:12 +0600

    Updated mongoose

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = 3006;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app
+    .listen(port)
+    .then(() => console.info(`App running at port: ${port}`));
 }
 bootstrap();
